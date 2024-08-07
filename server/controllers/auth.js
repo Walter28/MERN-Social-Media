ea0699757b8250301029381ab7391d7d80e5b207
Walter28 <walterchristian28@gmail.com>
2024-08-08 01:58:04 +0200

    user register done

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,40 @@
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 import User from "../models/User.js"
+
+/* REGISTER USER */
+export const register = async (req, res) => {
+    try {
+        //in front end we must send datas in this format, these data will come from the frontend
+        const {
+            firstName,
+            lastname,
+            email,
+            password,
+            picturePath,
+            friends,
+            location,
+            occupation,
+        } = req.body
+
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            firstName,
+            lastname,
+            email,
+            password: passwordHash,
+            picturePath,
+            friends,
+            location,
+            occupation,
+            viewedProfile: Math.floor(Math.random() * 10000),
+            impressions: Math.floor(Math.random() * 10000),
+        })
+        const savedUser = await newUser.save()
+        res.status(201).json(savedUser)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+}
